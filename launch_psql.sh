f3f1f247e97220df79d7e7a79e4490c43610f39d
Victor <vitcheffvi@cy-tech.fr>
2022-12-29 12:12:10 +0100

    update key postgresql

--- launch_psql.sh
+++ launch_psql.sh
@@ -1,1 +1,2 @@
-psql postgres://avnadmin:07bu6sBajn4IFix8mZOJ@postgresql-2791bab0-od486479f.database.cloud.ovh.net:20184/defaultdb?sslmode=require
+./env.sh
+psql postgres://$USERNAME:$PASSWORD@postgresql-2791bab0-od486479f.database.cloud.ovh.net:20184/defaultdb?sslmode=require
