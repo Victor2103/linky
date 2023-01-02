9f33c8a34df9b825fb11ec2edb0111117f3fe900
Victor <vitcheffvi@cy-tech.fr>
2023-01-02 17:26:24 +0100

    update script add the enedis gateway

--- script.py
+++ script.py
@@ -3,30 +3,50 @@ import requests
 from dotenv import load_dotenv
 import os
 import psycopg2
+import json
 
 # Load environments variables
 load_dotenv()
 
-# api-endpoint
-URL = f"https://www.myelectricaldata.fr/daily_consumption_max_power/{str(os.getenv('PDL'))}/start/2022-09-05/end/2022-10-12"
+"""
+# api-endpoint for my electrical data
+URL = f"https://www.myelectricaldata.fr/daily_consumption/{str(os.getenv('PDL'))}/start/2022-09-05/end/2022-10-12"
 # defining a params dict for the parameters to be sent to the API
 PARAMS = {"Authorization": str(os.getenv("TOKEN"))}
 # sending get request and saving the response as response object
 r = requests.get(url=URL, headers=PARAMS)
 # extracting data in json format
 data = r.json()
+"""
 
-print(data["meter_reading"]["interval_reading"][0])
+# api-endpoint for enedis gateway
+URL = "https://enedisgateway.tech/api"
+# Defining the headers
+PARAMS = {'Authorization': str(
+    os.getenv("ENEDIS_TOKEN")), 'Content-Type': "application/json"}
+# Defining the data to post
+data = {"type": "daily_consumption", "usage_point_id": str(
+    os.getenv('PDL')), "start": "2022-12-20", "end": "2022-12-31"}
+# sending post request and saving the response as response object
+r = requests.post(url=URL, headers=PARAMS, data=json.dumps(data))
+# extracting data in json format
+data = r.json()
 
 
-#Connect to the database
-connection=psycopg2.connect(f"postgres://{str(os.getenv('USERNAME'))}:{str(os.getenv('PASSWORD'))}@postgresql-2791bab0-od486479f.database.cloud.ovh.net:20184/electric?sslmode=require")
+# print(data)
+print(data["meter_reading"]["interval_reading"][0]["value"])
+print(data["meter_reading"]["interval_reading"][0]["date"])
+
+
+# Connect to the database
+connection = psycopg2.connect(
+    f"postgres://{str(os.getenv('USERNAME'))}:{str(os.getenv('PASSWORD'))}@postgresql-2791bab0-od486479f.database.cloud.ovh.net:20184/electric?sslmode=require")
 cursor = connection.cursor()
 
 # Make a query inside the database
-#cursor.execute("")
-#cursor.fetchone()
+# cursor.execute("")
+# cursor.fetchone()
 
-#Close the database
+# Close the database
 cursor.close()
 connection.close()
