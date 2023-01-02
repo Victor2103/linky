17e9c6680dade8e093eaa61adc9b20b0a5e07275
Victor <vitcheffvi@cy-tech.fr>
2023-01-02 18:03:52 +0100

    update launch_psql, connect to the good database

--- launch_psql.sh
+++ launch_psql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ~/.bashrc
-psql --no-password postgres://$USERNAME:$PASSWORD@postgresql-2791bab0-od486479f.database.cloud.ovh.net:20184/defaultdb?sslmode=require
+psql --no-password postgres://$USERNAME:$PASSWORD@postgresql-2791bab0-od486479f.database.cloud.ovh.net:20184/electric?sslmode=require
