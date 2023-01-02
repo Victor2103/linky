8dffa9eb782ea7a3cb21b8a346490c74efe6fb60
Victor <vitcheffvi@cy-tech.fr>
2023-01-02 18:24:56 +0100

    update script change the date to get

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 """
 # api-endpoint for my electrical data
-URL = f"https://www.myelectricaldata.fr/daily_consumption/{str(os.getenv('PDL'))}/start/2022-09-05/end/2022-10-12"
+URL = f"https://www.myelectricaldata.fr/daily_consumption/{str(os.getenv('PDL'))}/start/2022-12-29/end/2022-12-31"
 # defining a params dict for the parameters to be sent to the API
 PARAMS = {"Authorization": str(os.getenv("TOKEN"))}
 # sending get request and saving the response as response object
@@ -26,36 +26,43 @@ PARAMS = {'Authorization': str(
     os.getenv("ENEDIS_TOKEN")), 'Content-Type': "application/json"}
 # Defining the data to post
 data = {"type": "daily_consumption", "usage_point_id": str(
-    os.getenv('PDL')), "start": "2022-12-20", "end": "2022-12-31"}
+    os.getenv('PDL')), "start": "2022-12-29", "end": "2022-12-31"}
 # sending post request and saving the response as response object
 r = requests.post(url=URL, headers=PARAMS, data=json.dumps(data))
 # extracting data in json format
 data = r.json()
 
-
-#print(data)
-print(data["meter_reading"]["interval_reading"][0]["value"])
-print(data["meter_reading"]["interval_reading"][0]["date"])
-
-#Save the date and the value
-date=data["meter_reading"]["interval_reading"][0]["date"]
-value=data["meter_reading"]["interval_reading"][0]["value"]
+print(data)
+# print(data["meter_reading"]["interval_reading"][0]["value"])
+# print(data["meter_reading"]["interval_reading"][0]["date"])
 
 # Connect to the database
 connection = psycopg2.connect(
     f"postgres://{str(os.getenv('USERNAME'))}:{str(os.getenv('PASSWORD'))}@postgresql-2791bab0-od486479f.database.cloud.ovh.net:20184/electric?sslmode=require")
 cursor = connection.cursor()
 
+# We make a loop and we save all the data in the database
+for i in range(0, len(data["meter_reading"]["interval_reading"])):
+    date = data["meter_reading"]["interval_reading"][i]["date"]
+    value = data["meter_reading"]["interval_reading"][i]["value"]
+    # Make a query inside the database and save the value of the consommation
+    cursor.execute(
+        "INSERT INTO production_real_time (time,value) VALUES (%s,%s); ", (date, value))
+
+
 # Make a query inside the database and save one value of the electricity
-cursor.execute("INSERT INTO production_real_time (time,value) VALUES (%s,%s); ",(date,value))
+cursor.execute(
+    "INSERT INTO production_real_time (time,value) VALUES (%s,%s); ", (date, value))
 
 # Show if the query has worked
 cursor.execute("SELECT * FROM production_real_time ;")
-#Use fettch one to print only the value you add
+# Use fettch one to print only the value you add
 print(cursor.fetchone())
 
+"""
 # This is for make the data saved in the database. 
 connection.commit()
+"""
 
 # Close the database
 cursor.close()
