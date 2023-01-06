5be37801c4cfed371f35ca0cbacdc23bd1089a78
Victor <vitcheffvi@cy-tech.fr>
2023-01-06 21:20:25 +0100

    [average_month.py] Add the value to the db

--- average_month.py
+++ average_month.py
@@ -22,3 +22,21 @@ cursor.execute(
     "SELECT COUNT(conso) FROM consommation WHERE time > '2022-11-30 23:59:00' AND time < '2023-01-01 00:00:00'")
 decembre = cursor.fetchone()
 print(octobre, novembre, decembre)
+
+# We empty the database
+cursor.execute("TRUNCATE conso_month;")
+
+# We add the value of the average in the database
+cursor.execute(
+    "INSERT INTO conso_month (month,value) VALUES (%s,%s)", ("octobre", octobre))
+cursor.execute(
+    "INSERT INTO conso_month (month,value) VALUES (%s,%s)", ("novembre", novembre))
+cursor.execute(
+    "INSERT INTO conso_month (month,value) VALUES (%s,%s)", ("decembre", decembre))
+
+# This is for make the data saved in the database.
+connection.commit()
+
+# Close the database
+cursor.close()
+connection.close()
